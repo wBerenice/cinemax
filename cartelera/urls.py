de5332926401cc9f3b6fecf4db5357d66931ee6c
Berenice Sanchez <sanchezberenice09@gmailcom>
2025-06-24 10:06:00 -0600

    primer commit - el endpoint GET aún regresa corchetes vacíos, pero puedo correr el servidor sin ningún problema

---
+++ cartelera/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    CineListView,
+    PeliculaListView,
+    PeliculaDetailView,
+    HorarioListView,
+    FuncionListView,
+    UsuarioInteresadoListView
+)
+
+urlpatterns = [
+    path('cines/', CineListView.as_view()),
+    path('peliculas/', PeliculaListView.as_view()),
+    path('peliculas/<int:pelicula_id>/', PeliculaDetailView.as_view()), 
+    path('horarios/', HorarioListView.as_view()),
+    path('funciones/', FuncionListView.as_view()),
+    path('usuarios_interesados/', UsuarioInteresadoListView.as_view()),
+]
